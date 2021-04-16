ed33a09f6000f073b1f394573a2c2bc38875f53c
AugustRo <64295693+AugustRo@users.noreply.github.com>
2021-04-16 08:52:35 -0500

    Controllers improved

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,15 @@
+import PostMessage from '../models/postMessage.js';
+
 export const getPosts = (req,res)=> {
-    res.send('THIS WORKS!!!');
+    try {
+      const postMessages = await PostMessage.find();  
+      console.log(postMessages);
+      res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export cons createPost = (req, res) => {
+res.send('Creacion del post');
 }
